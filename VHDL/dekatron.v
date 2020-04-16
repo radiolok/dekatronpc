df3167dd770cdc8685a93c916de59d54ae7d33c2
Artem Kashkanov <radiolok@users.noreply.github.com>
2020-04-16 17:57:32 +0300

    Update Dekatron

--- VHDL/dekatron.v
+++ VHDL/dekatron.v
@@ -1,28 +1,32 @@
-module Dekatron(StepIn, Dir, Rst, Out, StepOut);
+module Dekatron(Step, Reverse, Rst, Set, In, Out);
     input wire Step;
-    input wire Dir;//1 for reverse
+    input wire Reverse;//1 for reverse
     input wire Rst;
-    inout Reg[9:0] Out;
-    output wire StepOut;
+    input wire Set;
+    input wire [9:0] In; 
+    output reg[9:0] Out;
 
-always @(posedge StepIn, negedge Rst)
+always @(posedge Step, negedge Rst)
     if (!Rst)
         begin
             Out <= 10'b1;
-            assign StepOut = 1'b0;
         end
     else
         begin
-            if (!Dir) 
-            begin 
-                Out <= {Out[8:0], Out[9]};
-                assign StepOut = Out[0];
-            end 
-            else 
-            begin 
-                Out <= {Out[9], Out[8:0]};
-                assign StepOut = Out[9];
-            end 
+            if (Set)
+                begin
+                    Out  <= In;
+                end
+            else
+                begin
+                    if (!Dir) 
+                    begin 
+                        Out <= {Out[8:0], Out[9]};
+                    end 
+                    else 
+                    begin 
+                        Out <= {Out[9], Out[8:0]};
+                    end 
+                end
         end
-
 endmodule
