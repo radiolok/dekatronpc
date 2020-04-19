59e6e8be6e651a2e62d61ad05e6a3727577f3929
radiolok <radiolok@yandex.ru>
2020-04-19 20:02:31 +0300

    use trigger logic

--- vhdl/dekatron.v
+++ vhdl/dekatron.v
@@ -6,18 +6,20 @@ module Dekatron(Step, Enable, Reverse, Rst_n, Set, In, Out);
     input wire Rst_n;
     input wire Set;
     input wire [9:0] In; 
-    output reg[9:0] Out;
+    output wire[9:0] Out;
 
-wire[3:0] signals = {Rst_n, Set, Enable, Reverse};
-	 
 always @(posedge Step or negedge Rst_n)
-	casez(signals)
-		4'b0???: Out <= 10'b0000000001;//Rst_n
-		4'b111?: Out <= In;//Set
-		4'b1010: Out <= {Out[8:0], Out[9]};//Enable forward
-		4'b1011: Out <= {Out[0], Out[9:1]};//Enable reverse
-	endcase
-	
-
-    
+	if (~Rst_n) Out <= 10'b0000000001;//Rst_n
+	else if (Enable)
+		if (Set) 
+			Out <= In;
+		else
+			if (Reverse)
+				Out <= {Out[0], Out[9:1]};//Enable reverse
+			else
+				Out <= {Out[8:0], Out[9]};//Enable forward
+		    
 endmodule
+
+		
+		
