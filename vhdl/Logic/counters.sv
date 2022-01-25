4075a7d0de28d5aa93fdfab852c5f505abb995b2
Artem Kashkanov <radiolok@yandex.ru>
2022-01-26 00:15:15 +0300

    Parametrized Clock divider

--- vhdl/Logic/counters.sv
+++ vhdl/Logic/counters.sv
@@ -1,14 +1,16 @@
 // fpga4student.com: FPGA projects, VHDL projects, Verilog projects
 // Verilog project: Verilog code for clock divider on FPGA
 // Top level Verilog code for clock divider on FPGA
-module Clock_divider(
+module Clock_divider #(
+    parameter DIVISOR = 28'd2,
+    parameter DUTY_CYCLE=50 //percents
+)(
     input wire Rst_n,
     input wire clock_in,
     output reg clock_out
     );
 
 reg[27:0] counter=28'd0;
-parameter DIVISOR = 28'd2;
 // The frequency of the output clk_out
 //  = The frequency of the input clk_in divided by DIVISOR
 // For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
@@ -23,7 +25,7 @@ always @(posedge clock_in, negedge Rst_n) begin
         counter <= counter + 28'd1;
         if(counter>=(DIVISOR-1))
         counter <= 28'd0;
-        clock_out <= (counter<DIVISOR/2)?1'b1:1'b0;            
+        clock_out <= (counter<((DIVISOR*DUTY_CYCLE)/100))? 1'b1: 1'b0;            
     end
 end
 endmodule
