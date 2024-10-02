affbffbbec7233802e6418e5ef54d6fe7886210c
Artem Kashkanov <radiolok@yandex.ru>
2024-10-02 09:08:17 +0300

    mem has 10 banks (commented out)

--- vhdl/DekatronPC/IpMemory.sv
+++ vhdl/DekatronPC/IpMemory.sv
@@ -24,16 +24,18 @@ wire [INSN_WIDTH-1: 0] RomOutWire;
 reg [INSN_WIDTH-1: 0] RomOutReg;
 reg [INSN_WIDTH-1: 0] RamOutReg;
 
-localparam IP_RAM_BIN_BW = $clog2(ROWS-1);
+localparam IP_RAM_BIN_BW = $clog2(ROWS/10-1);
 wire [IP_RAM_BIN_BW-1:0] AddressBin;
 
 BcdToBinEnc #(
-    .DIGITS(IP_DEKATRON_NUM),
+    .DIGITS(IP_DEKATRON_NUM-1),
     .OUT_WIDTH(IP_RAM_BIN_BW)
 ) ApRAM_address_enc (
-    .bcd(Address),
+    .bcd(Address[(IP_DEKATRON_NUM-1)*DEKATRON_WIDTH-1:0]),
     .bin(AddressBin)
 );
+
+wire [3:0] mem_bank = Address[IP_DEKATRON_NUM*DEKATRON_WIDTH-1:(IP_DEKATRON_NUM-1)*DEKATRON_WIDTH];
 
 parameter [1:0]
     INIT      = 2'd0,
@@ -78,9 +80,27 @@ assign Ready = ~Request & (state == READY);
 
 assign InsnOut = (isBootloader) ? RomOutReg : RamOutReg;
 
-reg [INSN_WIDTH-1:0] Mem [0:ROWS-1];
+reg [INSN_WIDTH-1:0] Mem0 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem1 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem2 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem3 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem4 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem5 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem6 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem7 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem8 [0:ROWS/10-1];
+// reg [INSN_WIDTH-1:0] Mem9 [0:ROWS/10-1];
 initial begin
-    $readmemh("../firmware.hex", Mem);
+    $readmemh("../firmware.hex", Mem0);
+    // $readmemh("../firmware.hex", Mem1);
+    // $readmemh("../firmware.hex", Mem2);
+    // $readmemh("../firmware.hex", Mem3);
+    // $readmemh("../firmware.hex", Mem4);
+    // $readmemh("../firmware.hex", Mem5);
+    // $readmemh("../firmware.hex", Mem6);
+    // $readmemh("../firmware.hex", Mem7);
+    // $readmemh("../firmware.hex", Mem8);
+    // $readmemh("../firmware.hex", Mem9);        
 end
 
 bootloader #(
@@ -94,9 +114,43 @@ always @(posedge Clk, negedge Rst_n) begin
       RamOutReg <= {INSN_WIDTH{1'b0}};
       RomOutReg <= {(INSN_WIDTH){1'b0}};
     end
-    else if (WE) Mem[AddressBin] <= InsnIn;
-      else begin 
-        RamOutReg <= Mem[AddressBin];
+    else if (WE) Mem0[AddressBin] <= InsnIn;
+      else begin
+        case(mem_bank)
+            4'h0: begin
+                RamOutReg <= Mem0[AddressBin];
+            end
+            // 4'h1: begin
+            //     RamOutReg <= Mem1[AddressBin];
+            // end
+            // 4'h2: begin
+            //     RamOutReg <= Mem2[AddressBin];
+            // end
+            // 4'h3: begin
+            //     RamOutReg <= Mem3[AddressBin];
+            // end
+            // 4'h4: begin
+            //     RamOutReg <= Mem4[AddressBin];
+            // end
+            // 4'h5: begin
+            //     RamOutReg <= Mem5[AddressBin];
+            // end
+            // 4'h6: begin
+            //     RamOutReg <= Mem6[AddressBin];
+            // end
+            // 4'h7: begin
+            //     RamOutReg <= Mem7[AddressBin];
+            // end
+            // 4'h8: begin
+            //     RamOutReg <= Mem8[AddressBin];
+            // end
+            // 4'h9: begin
+            //     RamOutReg <= Mem9[AddressBin];
+            // end
+            default: begin
+                RamOutReg <= Mem0[AddressBin];
+            end
+        endcase
         RomOutReg <= RomOutWire;
     end
 end
@@ -104,12 +158,13 @@ end
 `ifdef EMULATOR
     wire [IP_RAM_BIN_BW-1:0] Address1Bin;
     BcdToBinEnc #(
-        .DIGITS(IP_DEKATRON_NUM),
+        .DIGITS(IP_DEKATRON_NUM-1),
         .OUT_WIDTH(IP_RAM_BIN_BW)
     ) ApRAM1_address_enc (
-        .bcd(Address1),
+        .bcd(Address1[(IP_DEKATRON_NUM-1)*DEKATRON_WIDTH-1:0]),
         .bin(Address1Bin)
     );
+    wire [3:0] mem_bank_1 = Address1[IP_DEKATRON_NUM*DEKATRON_WIDTH-1:(IP_DEKATRON_NUM-1)*DEKATRON_WIDTH];
     wire [INSN_WIDTH-1: 0] RomOutWire1;
     reg [INSN_WIDTH-1: 0] RomOutReg1;
     reg [INSN_WIDTH-1: 0] RamOutReg1;
@@ -127,7 +182,41 @@ end
             RomOutReg1 <= {(INSN_WIDTH){1'b0}};
         end
         else begin 
-            RamOutReg1 <= Mem[Address1Bin];
+            case(mem_bank_1)
+            4'h0: begin
+                RamOutReg1 <= Mem0[Address1Bin];
+            end
+            // 4'h1: begin
+            //     RamOutReg1 <= Mem1[Address1Bin];
+            // end
+            // 4'h2: begin
+            //     RamOutReg1 <= Mem2[Address1Bin];
+            // end
+            // 4'h3: begin
+            //     RamOutReg1 <= Mem3[Address1Bin];
+            // end
+            // 4'h4: begin
+            //     RamOutReg1 <= Mem4[Address1Bin];
+            // end
+            // 4'h5: begin
+            //     RamOutReg1 <= Mem5[Address1Bin];
+            // end
+            // 4'h6: begin
+            //     RamOutReg1 <= Mem6[Address1Bin];
+            // end
+            // 4'h7: begin
+            //     RamOutReg1 <= Mem7[Address1Bin];
+            // end
+            // 4'h8: begin
+            //     RamOutReg1 <= Mem8[Address1Bin];
+            // end
+            // 4'h9: begin
+            //     RamOutReg1 <= Mem9[Address1Bin];
+            // end
+            default: begin
+                RamOutReg1 <= Mem0[Address1Bin];
+            end
+        endcase
             RomOutReg1 <= RomOutWire1;
         end
     end
--- vhdl/run/run_emul.sh
+++ vhdl/run/run_emul.sh
@@ -38,14 +38,18 @@ DPCfiles=$(cat ${root_dir}/DekatronPC/DPC.files)
 
 EmulFiles=$(cat ${root_dir}/Emulator/Emul.files)
 
-bf_file=${root_dir}/programs/helloworld/helloworld.bfk
-#bf_file=${root_dir}/programs/pi/pi.bfk
-#bf_file=programs/rot13/rot13.bfk
+bf_file="${root_dir}/programs/helloworld/helloworld.bfk"
+#bf_file="${bf_file} ${root_dir}/programs/fibonachi/fibonachi.bfk"
+#bf_file="${bf_file} ${root_dir}/programs/pi/pi.bfk"
+#bf_file="${bf_file} ${root_dir}/programs/rot13/rot13.bfk"
+#bf_file="${bf_file} ${root_dir}/programs/fractal/fractal.bfk"
+python ${root_dir}/programs/generate_rom.py -f ${bf_file} -o ${root_dir}/firmware.hex --hex
+python ${root_dir}/programs/generate_rom.py -f ${bf_file} -o ${root_dir}/firmware.sv 
 
 verilator -Wall --trace --top Emulator --clk FPGA_CLK_50 --cc ${EmulFiles} ${DPCfiles} \
 -GDIVIDE_TO_01US=1 --timescale 1us/10ns  +define+EMULATOR -DVERILATOR=1 \
 --exe ${root_dir}/DekatronPC/tests/Emulator.sv/Emulator_tb.cpp -LDFLAGS -lncurses
 
 make -j`nproc` -C obj_dir -f VEmulator.mk VEmulator
-python ${root_dir}/programs/generate_rom.py -f ${bf_file} --hex -o ${root_dir}/firmware.hex
+
 ./obj_dir/VEmulator
