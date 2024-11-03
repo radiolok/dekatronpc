58f0222149ec97706aada6ffeb080cf3dd7e250e
Artem Kashkanov <radiolok@yandex.ru>
2024-11-03 12:45:19 +0300

    Consul: works much better

--- vhdl/Emulator/consul.sv
+++ vhdl/Emulator/consul.sv
@@ -11,9 +11,10 @@ module consul(
     output wire print_data_rdy
 );
 
-localparam PERIOD_SYMBOL = 4;
-localparam PERIOD_NL = 500;
-localparam PERIOD_COLOR = 12;
+localparam PERIOD_SYMBOL = 5;
+localparam PERIOD_PAUSE = 5;
+localparam PERIOD_NL = 10;
+//localparam PERIOD_COLOR = 12;
 /* Consul 260 section */
 
 //Output lines
@@ -29,12 +30,12 @@ reg [7:0] in;//11L-18L
 reg need_nl;//11R
 reg block_print;//12R
 reg is_moving;//13R
-reg high_reg;//14R
 /* verilator lint_off UNUSEDSIGNAL */
+reg high_reg;//14R
 reg coAcq;//15R
 reg red_print;//16R
 wire dummy_stdout = print_data_i[7];
-reg top_symbol_correction;//17R
+reg low_reg;//17R
 reg cin_ready;//18R
 /* verilator lint_on UNUSEDSIGNAL */
 
@@ -44,7 +45,7 @@ always @(posedge Clk, negedge Rst_n) begin
         regs_out <= 10'b0;
         {
             cin_ready,
-            top_symbol_correction,
+            low_reg,
             red_print,
             coAcq,
             high_reg,
@@ -63,7 +64,7 @@ always @(posedge Clk, negedge Rst_n) begin
         };
         {
             cin_ready,
-            top_symbol_correction,
+            low_reg,
             red_print,
             coAcq,
             high_reg,
@@ -81,7 +82,8 @@ parameter [3:0]
     IDLE        = 4'b0000,
     WAIT        = 4'b0001,
     SET_CODE    = 4'b0010,
-    SET_SYNC    = 4'b0011;
+    SET_SYNC    = 4'b0011,
+    SET_PAUSE   = 4'b0100;
 
 assign print_data_rdy = (state == IDLE);
 
@@ -118,26 +120,52 @@ always @(posedge Clk, negedge Rst_n) begin
                 end
             end
             WAIT: begin
-                if (~is_moving & ~block_print) begin
+                if (is_moving | block_print) begin
+                    state <= WAIT;
+                end else begin
                     if (need_nl) begin
                         out <= 7'h0d;//CR
                         srv_symbol <= 1'b1;
                         high_reg_counter <= PERIOD_NL;
                     end
                     else begin
-                        if (high_reg ^ ~print_data[5]) begin
-                            high_reg_counter <= PERIOD_COLOR;
-                            out <= {6'b000111, print_data[5]};
-                            srv_symbol <= 1'b1;
-                        end else begin
-                            high_reg_counter <= PERIOD_SYMBOL;
-                            out <= print_data[6:0];
+                        //  if (high_reg ^ ~print_data[5]) begin
+                            //  high_reg_counter <= PERIOD_COLOR;
+                            //  out <= {6'b000111, print_data[5]};
+                            //  srv_symbol <= 1'b1;
+                        //  end else begin                        
                             srv_symbol <= 1'b0;
-                        end
+                            case (print_data[6:0])
+                                7'h0a: begin
+                                    out <= 7'h0d;
+                                    high_reg_counter <= PERIOD_NL;
+                                end
+                                7'h0d: begin 
+                                    out <= 7'h0d;
+                                    high_reg_counter <= PERIOD_NL;
+                                end
+                                7'h20: begin //space
+                                    out <= 7'h5F;
+                                    high_reg_counter <= PERIOD_SYMBOL;
+                                end
+                                7'h30: begin //0
+                                    out <= 7'h3F;
+                                    high_reg_counter <= PERIOD_SYMBOL;
+                                end
+                                default: begin
+                                    case (print_data[6:4])
+                                        3'h2: out[6:4] <= 3'h3;
+                                        3'h6: out[6:4] <= 3'h4;
+                                        3'h7: out[6:4] <= 3'h5;
+                                        default: out[6:4] <= print_data[6:4];
+                                    endcase
+                                    out[3:0] <= print_data[3:0];
+                                    high_reg_counter <= PERIOD_SYMBOL;
+                                end
+                            endcase
+                        //end
                     end
                     state <= SET_CODE;
-                end else begin
-                    state <= WAIT;
                 end
             end
             SET_CODE: begin//10ms delay for relays
@@ -148,7 +176,17 @@ always @(posedge Clk, negedge Rst_n) begin
                 if (high_reg_counter == 0) begin
                     sync <= 1'b0;
                     out <= '0;
-                    state <= (srv_symbol)? WAIT : IDLE;
+                    if (srv_symbol) begin
+                        state <= WAIT;
+                    end else begin
+                        state <= SET_PAUSE;
+                        high_reg_counter <= (out == 7'h0d) ? PERIOD_NL : PERIOD_PAUSE;
+                    end
+                end
+            end
+            SET_PAUSE: begin
+                if (high_reg_counter == 0) begin
+                    state <= IDLE;
                 end
             end
             default:
@@ -156,5 +194,4 @@ always @(posedge Clk, negedge Rst_n) begin
         endcase
     end
 end
-
 endmodule
